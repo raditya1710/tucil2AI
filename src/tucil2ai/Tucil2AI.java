1b52f0f522ac4eccbd704aea34b45d095d639bd9
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-11-02 09:37:34 +0700

    Adding javadoc above function and procedures

--- src/tucil2ai/Tucil2AI.java
+++ src/tucil2ai/Tucil2AI.java
@@ -8,7 +8,6 @@ package tucil2ai;
 import java.util.Random;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.core.Utils;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
@@ -22,7 +21,9 @@ import weka.filters.supervised.attribute.Discretize;
 
 public class Tucil2AI {
     /**
-     * @param args the command line arguments
+     * @param filename
+     * @return 
+     * @throws java.lang.Exception
      */
     protected static Instances load(String filename) throws Exception{
         DataSource source = new DataSource(filename);
@@ -31,7 +32,13 @@ public class Tucil2AI {
         data.setClassIndex(data.numAttributes() - 1);
         return data;
     }
-    
+    /**
+     *
+     * @param data
+     * @param cross
+     * @return
+     * @throws Exception
+     */
     protected static Evaluation evalJ48(Instances data, boolean cross) throws Exception{
         Evaluation E;
         E = new Evaluation(data);
@@ -46,12 +53,29 @@ public class Tucil2AI {
         return E;
     }
     
+    /**
+     *
+     * @param E
+     * @throws Exception
+     */
+    public static void printEval(Evaluation E) throws Exception{
+        System.out.println(E.toSummaryString("\nResults\n======\n", false));
+        System.out.println(E.toClassDetailsString());
+        System.out.println(E.toMatrixString());
+    }
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         DataSource source;
         Instances data;
         Instances data_filtered;
         Discretize filter;
+        
+        // input files
         source = new DataSource("C:/Program Files/Weka-3-8/data/iris.arff");
         data = source.getDataSet();
         data.setClassIndex(data.numAttributes() - 1);
@@ -61,10 +85,9 @@ public class Tucil2AI {
         filter.setInputFormat(data);
         data_filtered = Filter.useFilter(data, filter);
         
+        //Evaluation with J48 Decision Tree
         Evaluation eval = evalJ48(data_filtered, true);
         
-        System.out.println(eval.toSummaryString("\nResults\n======\n", false));
-        System.out.println(eval.toClassDetailsString());
-        System.out.println(eval.toMatrixString());
+        printEval(eval);
     }
 }
